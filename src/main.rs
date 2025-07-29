07f27b742217999c52811b8cb25412ccf4bbdd80
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-07-28 22:55:59 -0300

    feat: adds redis connection to actix shared state & pushes payment payload to payments redis queue

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::env;
 
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use chrono::{DateTime, Utc};
-use serde::Deserialize;
+use dotenv::dotenv;
+use redis::Client;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct CreatePaymentBody {
     #[serde(rename = "correlationId")]
     correlation_id: String,
@@ -12,15 +14,26 @@ struct CreatePaymentBody {
 }
 
 #[post("/payments")]
-async fn create_payment(payload: actix_web::web::Json<CreatePaymentBody>) -> impl Responder {
-    let data = payload.into_inner();
+async fn create_payment(
+    payload: web::Json<CreatePaymentBody>,
+    redis_connection: web::Data<redis::aio::MultiplexedConnection>,
+) -> impl Responder {
+    let json_string = match serde_json::to_string(&payload.into_inner()) {
+        Ok(val) => val,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
 
-    print!(
-        "Received payment request: correlation_id={}, amount={}",
-        data.correlation_id, data.amount
-    );
+    let mut connection = redis_connection.as_ref().clone();
+    let lpush_result: redis::RedisResult<()> = redis::cmd("LPUSH")
+        .arg("@payments_queue")
+        .arg(json_string)
+        .query_async(&mut connection)
+        .await;
 
-    HttpResponse::Created()
+    match lpush_result {
+        Ok(_) => HttpResponse::Created().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
 }
 
 #[derive(Deserialize)]
@@ -30,9 +43,7 @@ struct PaymentsSummaryQuery {
 }
 
 #[get("/payments-summary")]
-async fn get_payments_summary(
-    query: actix_web::web::Query<PaymentsSummaryQuery>,
-) -> impl Responder {
+async fn get_payments_summary(query: web::Query<PaymentsSummaryQuery>) -> impl Responder {
     let from = query.from;
     let to = query.to;
 
@@ -41,15 +52,28 @@ async fn get_payments_summary(
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     let api_port = env::var("API_PORT").expect("API_PORT env must be set");
+    let redis_url = env::var("REDIS_URL").expect("REDIS_URL env must be set");
+
+    let redis_client = Client::open(redis_url).expect("Failed to create Redis client");
+
+    let redis_connection = redis_client
+        .get_multiplexed_tokio_connection()
+        .await
+        .expect("Failed to connect to Redis");
+
+    let redis_connection = web::Data::new(redis_connection);
 
     println!(
         "{}",
         format!("🦀 Server started successfully on port {api_port}")
     );
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(redis_connection.clone())
             .service(create_payment)
             .service(get_payments_summary)
     })
