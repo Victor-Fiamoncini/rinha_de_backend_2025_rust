d540888eb6248aa46fda6efdb184c24f31b7fd3a
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-08-09 23:05:49 -0300

    feat: adds queue mod with redis connection manager

---
+++ src/queue/queue.rs
@@ -0,0 +1,30 @@
+use redis::{aio::ConnectionManager, AsyncCommands, Client};
+
+use crate::config::Config;
+
+#[derive(Clone)]
+pub struct Queue {
+    connection: ConnectionManager,
+    name: &'static str,
+}
+
+impl Queue {
+    pub async fn new(config: Config, name: &'static str) -> Self {
+        let client = Client::open(config.redis_url).expect("Failed to create Redis Client");
+
+        let connection = ConnectionManager::new(client)
+            .await
+            .expect("Failed to create Redis ConnectionManager");
+
+        Queue { connection, name }
+    }
+
+    pub async fn enqueue(&self, message: String) -> Result<(), &'static str> {
+        let mut connection = self.connection.clone();
+
+        match connection.rpush::<_, _, ()>(&self.name, message).await {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Failed to enqueue message on Redis queue"),
+        }
+    }
+}
