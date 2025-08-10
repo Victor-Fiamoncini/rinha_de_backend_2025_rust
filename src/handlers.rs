7e94dd45245edafc45c927e0f53f39d52d78706b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-08-09 23:07:11 -0300

    feat: creates axum server with services app state

---
+++ src/handlers.rs
@@ -0,0 +1,36 @@
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+
+use crate::{
+    types::{CreatePaymentDTO, GetPaymentsSummaryDTO},
+    AppState,
+};
+
+pub async fn create_payment(
+    State(state): State<AppState>,
+    Json(payment): Json<CreatePaymentDTO>,
+) -> impl IntoResponse {
+    match state
+        .services
+        .create_payment_service
+        .create_payment(payment)
+        .await
+    {
+        Ok(_) => StatusCode::CREATED,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
+pub async fn get_payments_summary(
+    State(state): State<AppState>,
+    query: Query<GetPaymentsSummaryDTO>,
+) -> impl IntoResponse {
+    let from = query.from;
+    let to = query.to;
+
+    ""
+}
--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,49 @@
-use std::env;
+mod config;
+mod handlers;
+mod queue;
+mod services;
+mod types;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use chrono::{DateTime, Utc};
+use std::sync::Arc;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use dotenv::dotenv;
-use redis::Client;
-use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-struct CreatePaymentBody {
-    #[serde(rename = "correlationId")]
-    correlation_id: String,
-    amount: f64,
+use crate::{
+    config::Config,
+    handlers::{create_payment, get_payments_summary},
+    queue::Queue,
+    services::Services,
+};
+
+#[derive(Clone)]
+pub struct AppState {
+    services: Arc<Services>,
 }
 
-#[post("/payments")]
-async fn create_payment(
-    payload: web::Json<CreatePaymentBody>,
-    redis_connection: web::Data<redis::aio::MultiplexedConnection>,
-) -> impl Responder {
-    let json_string = match serde_json::to_string(&payload.into_inner()) {
-        Ok(val) => val,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let mut connection = redis_connection.as_ref().clone();
-    let lpush_result: redis::RedisResult<()> = redis::cmd("LPUSH")
-        .arg("@payments_queue")
-        .arg(json_string)
-        .query_async(&mut connection)
-        .await;
-
-    match lpush_result {
-        Ok(_) => HttpResponse::Created().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
-}
-
-#[derive(Deserialize)]
-struct PaymentsSummaryQuery {
-    from: DateTime<Utc>,
-    to: DateTime<Utc>,
-}
-
-#[get("/payments-summary")]
-async fn get_payments_summary(query: web::Query<PaymentsSummaryQuery>) -> impl Responder {
-    let from = query.from;
-    let to = query.to;
-
-    HttpResponse::Ok().body(format!("Payments summary from {} to {}", from, to))
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
 
-    let api_port = env::var("API_PORT").expect("API_PORT env must be set");
-    let redis_url = env::var("REDIS_URL").expect("REDIS_URL env must be set");
+    let config = Config::new();
 
-    let redis_client = Client::open(redis_url).expect("Failed to create Redis client");
+    let payments_queue = Queue::new(config.clone(), "@pending_payments_queue").await;
 
-    let redis_connection = redis_client
-        .get_multiplexed_tokio_connection()
+    let app_state = AppState {
+        services: Arc::new(Services::new(payments_queue)),
+    };
+
+    let app = Router::new()
+        .route("/payments", post(create_payment))
+        .route("/payments-summary", get(get_payments_summary))
+        .with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", config.api_port))
         .await
-        .expect("Failed to connect to Redis");
+        .unwrap();
 
-    let redis_connection = web::Data::new(redis_connection);
-
-    println!(
-        "{}",
-        format!("🦀 Server started successfully on port {api_port}")
-    );
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(redis_connection.clone())
-            .service(create_payment)
-            .service(get_payments_summary)
-    })
-    .bind(format!("0.0.0.0:{api_port}"))?
-    .run()
-    .await
+    axum::serve(listener, app).await.unwrap();
 }
