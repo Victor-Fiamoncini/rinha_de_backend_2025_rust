f09025c5603f4477e1b7f7946dbd296dcece6713
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-08-24 11:26:04 -0300

    feat: add cargo workspace config and sh script env vars

--- run_dev_rinha_api.sh
+++ run_dev_rinha_api.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-API_PORT=3000 REDIS_URL=redis://127.0.0.1:6379 cargo run -p rinha_api
+export API_PORT=3000
+export REDIS_URL=redis://127.0.0.1:6379
+
+cargo run -p rinha_api
--- run_dev_rinha_worker.sh
+++ run_dev_rinha_worker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-PAYMENT_PROCESSOR_URL_DEFAULT=http://payment-processor-default:8080 \
-PAYMENT_PROCESSOR_URL_FALLBACK=http://payment-processor-fallback:8080 \
-REDIS_URL=redis://127.0.0.1:6379 \
+export PAYMENT_PROCESSOR_DEFAULT_URL=http://payment-processor-default:8080
+export PAYMENT_PROCESSOR_FALLBACK_URL=http://payment-processor-fallback:8080
+export REDIS_URL=redis://127.0.0.1:6379
+
 cargo run -p rinha_worker
