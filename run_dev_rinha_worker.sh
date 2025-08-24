486502347b15aab3f9627ba58877e2c0a8b382d7
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-08-24 17:13:56 -0300

    chore: adds docker files with ssh libraries & updates docker-compose/nginx and gitignore files

--- run_dev_rinha_worker.sh
+++ run_dev_rinha_worker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export PAYMENT_PROCESSOR_DEFAULT_URL=http://payment-processor-default:8080
-export PAYMENT_PROCESSOR_FALLBACK_URL=http://payment-processor-fallback:8080
+export PAYMENT_PROCESSOR_DEFAULT_URL=http://127.0.0.1:8001
+export PAYMENT_PROCESSOR_FALLBACK_URL=http://127.0.0.1:8002
 export REDIS_URL=redis://127.0.0.1:6379
 
 cargo run -p rinha_worker
