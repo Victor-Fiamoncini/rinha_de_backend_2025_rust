dc839b11a20c2c91428733380aa326198ca011f4
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-08-31 19:15:02 -0300

    feat: add PostgreSQL environment variables to development scripts

--- run_dev_rinha_api.sh
+++ run_dev_rinha_api.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 export API_PORT=3000
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT=5432
+export POSTGRES_DB=postgres
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
 export REDIS_URL=redis://127.0.0.1:6379
 
 cargo run -p rinha_api
--- run_dev_rinha_worker.sh
+++ run_dev_rinha_worker.sh
@@ -2,6 +2,11 @@
 
 export PAYMENT_PROCESSOR_DEFAULT_URL=http://127.0.0.1:8001
 export PAYMENT_PROCESSOR_FALLBACK_URL=http://127.0.0.1:8002
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT=5432
+export POSTGRES_DB=postgres
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
 export REDIS_URL=redis://127.0.0.1:6379
 
 cargo run -p rinha_worker
