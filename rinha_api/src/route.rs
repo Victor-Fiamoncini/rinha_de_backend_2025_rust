6b9a43959709b574d7fc6ef469e3b696b72ead59
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-09-01 20:04:38 -0300

    feat: refactor payment routes to streamline service execution and response handling

--- rinha_api/src/route.rs
+++ rinha_api/src/route.rs
@@ -4,6 +4,7 @@ use axum::{
     response::IntoResponse,
     Json,
 };
+use serde_json::json;
 
 use crate::{
     dto::{CreatePaymentDTO, GetPaymentsSummaryDTO},
@@ -14,12 +15,7 @@ pub async fn create_payment(
     State(state): State<AppState>,
     Json(payment): Json<CreatePaymentDTO>,
 ) -> impl IntoResponse {
-    match state
-        .services
-        .create_payment_service
-        .create_payment(payment)
-        .await
-    {
+    match state.services.create_payment_service.execute(payment).await {
         Ok(_) => StatusCode::CREATED,
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
@@ -29,13 +25,11 @@ pub async fn get_payments_summary(
     State(state): State<AppState>,
     query: Query<GetPaymentsSummaryDTO>,
 ) -> impl IntoResponse {
-    match state
+    let summary = state
         .services
         .get_payment_summary_service
-        .get_payment_summary(query.from, query.to)
-        .await
-    {
-        Ok(summary) => Ok(Json(serde_json::to_value(summary).unwrap())),
-        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
-    }
+        .execute(query.from.clone(), query.to.clone())
+        .await;
+
+    Json(json!(summary))
 }
