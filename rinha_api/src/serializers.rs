0cf8f4cd54f2dfa566291b5048ec5098d7ec3a57
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-09-01 20:03:36 -0300

    feat: add serializer for Decimal type to convert to f64

---
+++ rinha_api/src/serializers.rs
@@ -0,0 +1,12 @@
+use rust_decimal::prelude::ToPrimitive;
+use rust_decimal::Decimal;
+use serde::Serializer;
+
+pub fn serialize<S>(decimal: &Decimal, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let float_val = decimal.to_f64().unwrap_or(0.0);
+
+    serializer.serialize_f64(float_val)
+}
