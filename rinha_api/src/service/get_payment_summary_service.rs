2399c7ae02c64810944257daf8f7d8e5cd6a318e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-09-01 20:04:07 -0300

    feat: refactor GetPaymentSummaryService to use database for payment summary queries and improve date handling

--- rinha_api/src/service/get_payment_summary_service.rs
+++ rinha_api/src/service/get_payment_summary_service.rs
@@ -1,71 +1,139 @@
-use chrono::{DateTime, Utc};
-use tracing::info;
+use chrono::{DateTime, NaiveDateTime};
+use tokio_postgres::{types::ToSql, Row};
+use tracing::error;
 
 use crate::{
-    dto::{CompletedPaymentDTO, PaymentSummaryDTO, ProcessorSummaryDTO},
-    queue::Queue,
+    database::Database,
+    dto::{PaymentMetricDTO, PaymentSummaryDTO},
 };
 
 #[derive(Clone)]
 pub struct GetPaymentSummaryService {
-    queue: Queue,
+    database: Database,
 }
 
 impl GetPaymentSummaryService {
-    pub fn new(queue: Queue) -> Self {
-        GetPaymentSummaryService { queue }
+    pub fn new(database: Database) -> Self {
+        GetPaymentSummaryService { database }
     }
 
-    pub async fn get_payment_summary(
+    fn create_summary_query(
         &self,
-        from: Option<DateTime<Utc>>,
-        to: Option<DateTime<Utc>>,
-    ) -> Result<PaymentSummaryDTO, &'static str> {
-        let messages = self.queue.get_all().await?;
+        from: &Option<String>,
+        to: &Option<String>,
+    ) -> (String, Vec<NaiveDateTime>) {
+        let mut query = String::from(
+            "SELECT payment_processor, COUNT(1) as count, SUM(amount) as total_amount FROM payments",
+        );
 
-        let mut default_total_requests = 0;
-        let mut default_total_amount = 0.0;
-        let mut fallback_total_requests = 0;
-        let mut fallback_total_amount = 0.0;
+        let mut params = Vec::new();
+        let mut param_index = 1;
 
-        for message in messages {
-            match serde_json::from_str::<CompletedPaymentDTO>(&message) {
-                Ok(payment) => {
-                    if let Ok(payment_date) = DateTime::parse_from_rfc3339(&payment.created_at) {
-                        let payment_date_utc = payment_date.with_timezone(&Utc);
+        let mut from_parsed = None;
 
-                        if (from.is_none() || payment_date_utc >= from.unwrap())
-                            && (to.is_none() || payment_date_utc <= to.unwrap())
-                        {
-                            match payment.processor_name.as_str() {
-                                "default" => {
-                                    default_total_requests += 1;
-                                    default_total_amount += payment.amount;
-                                }
-                                "fallback" => {
-                                    fallback_total_requests += 1;
-                                    fallback_total_amount += payment.amount;
-                                }
-                                _ => {}
-                            }
-                        }
-                    }
+        if let Some(from_date) = from {
+            let parsed = DateTime::parse_from_rfc3339(&from_date)
+                .map(|dt| dt.naive_utc())
+                .or_else(|_| NaiveDateTime::parse_from_str(&from_date, "%Y-%m-%dT%H:%M:%S"));
+
+            match parsed {
+                Ok(dt) => {
+                    from_parsed = Some(dt);
                 }
                 Err(e) => {
-                    info!("Error parsing payment message: {}", e);
+                    error!("Failed to parse date `{}`: {}", from_date, e);
                 }
             }
         }
 
-        Ok(PaymentSummaryDTO {
-            default: ProcessorSummaryDTO {
-                total_requests: default_total_requests,
-                total_amount: default_total_amount,
-            },
-            fallback: ProcessorSummaryDTO {
-                total_requests: fallback_total_requests,
-                total_amount: fallback_total_amount,
-            },
-        })
+        let mut to_parsed = None;
+
+        if let Some(to_date) = to {
+            let parsed = DateTime::parse_from_rfc3339(&to_date)
+                .map(|dt| dt.naive_utc())
+                .or_else(|_| NaiveDateTime::parse_from_str(&to_date, "%Y-%m-%dT%H:%M:%S"));
+
+            match parsed {
+                Ok(dt) => {
+                    to_parsed = Some(dt);
+                }
+                Err(e) => {
+                    error!("Failed to parse date `{}`: {}", to_date, e);
+                }
+            }
+        }
+
+        if from_parsed.is_some() || to_parsed.is_some() {
+            query.push_str(" WHERE");
+
+            if let Some(parsed_date) = from_parsed {
+                query.push_str(&format!(" requested_at >= ${}", param_index));
+                params.push(parsed_date);
+                param_index += 1;
+            }
+
+            if let Some(parsed_date) = to_parsed {
+                if param_index > 1 {
+                    query.push_str(" AND");
+                }
+                query.push_str(&format!(" requested_at <= ${}", param_index));
+                params.push(parsed_date);
+            }
+        }
+
+        query.push_str(" GROUP BY payment_processor");
+
+        (query, params)
+    }
+
+    fn build_payment_summary(&self, rows: Vec<Row>) -> PaymentSummaryDTO {
+        let mut default_metric = PaymentMetricDTO {
+            total_requests: 0,
+            total_amount: Default::default(),
+        };
+
+        let mut fallback_metric = PaymentMetricDTO {
+            total_requests: 0,
+            total_amount: Default::default(),
+        };
+
+        for row in rows {
+            let payment_processor: String = row.get(0);
+
+            if payment_processor == "default" {
+                let requests: i64 = row.get(1);
+
+                default_metric.total_requests = requests.try_into().unwrap();
+                default_metric.total_amount = row.get(2);
+            } else {
+                let requests: i64 = row.get(1);
+
+                fallback_metric.total_requests = requests.try_into().unwrap();
+                fallback_metric.total_amount = row.get(2);
+            }
+        }
+
+        PaymentSummaryDTO {
+            default: default_metric,
+            fallback: fallback_metric,
+        }
+    }
+
+    pub async fn execute(&self, from: Option<String>, to: Option<String>) -> PaymentSummaryDTO {
+        let (query, params) = self.create_summary_query(&from, &to);
+
+        let rows: Vec<Row> = self
+            .database
+            .query(
+                &query,
+                &params
+                    .iter()
+                    .map(|param| param as &(dyn ToSql + Sync))
+                    .collect::<Vec<_>>(),
+            )
+            .await
+            .unwrap();
+
+        self.build_payment_summary(rows)
     }
 }
