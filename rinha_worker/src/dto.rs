0c26f385800b9181aea9e1ef1afffb9a2855040a
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-09-01 20:06:09 -0300

    feat: rename PendingPaymentDTO to PaymentDTO and update fields for consistency

--- rinha_worker/src/dto.rs
+++ rinha_worker/src/dto.rs
@@ -1,26 +1,20 @@
+use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct PendingPaymentDTO {
+pub struct PaymentDTO {
+    pub amount: Decimal,
+    #[serde(rename = "correlationId")]
+    pub correlation_id: String,
+    #[serde(default)]
+    pub payment_processor: String,
     #[serde(rename = "requestedAt")]
     #[serde(default)]
     pub requested_at: String,
-    #[serde(rename = "correlationId")]
-    pub correlation_id: String,
-    pub amount: f64,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Debug)]
 pub enum PaymentProcessor {
-    #[serde(rename = "default")]
     Default,
-    #[serde(rename = "fallback")]
     Fallback,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct CompletedPaymentDTO {
-    pub amount: f64,
-    pub processor_name: PaymentProcessor,
-    pub created_at: String,
 }
